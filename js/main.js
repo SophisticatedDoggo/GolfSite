dff855f9478633f6fa72d51e9c1d155bf64c4b6e
nolaw1555 <law1555@pennwest.edu>
2026-04-10 01:03:33 -0400

    Add mobile responsiveness and hamburger menu

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scrolled');
     }
 });
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('nav > ul');
+const navLinks = document.querySelectorAll('nav > ul > li > a')
+
+hamburger.addEventListener('click', function() {
+    if(navMenu.classList.contains('open')) {
+        navMenu.classList.remove('open')
+    } else (
+        navMenu.classList.toggle('open')
+    );
+    
+});
+
+navLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+        navMenu.classList.remove('open');
+    });
+    navMenu.classList.remove('open');
+});
